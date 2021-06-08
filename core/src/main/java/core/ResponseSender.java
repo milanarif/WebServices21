8e22634837c62f935680f90c3673813d88e912e2
Shorl <milanarif@gmail.com>
2021-06-08 22:21:33 +0200

    fixed response for post

--- core/src/main/java/core/ResponseSender.java
+++ core/src/main/java/core/ResponseSender.java
@@ -6,12 +6,20 @@ import java.nio.charset.StandardCharsets;
 
 public class ResponseSender {
     public static void sendResponse(OutputStream outputToClient, String response) throws IOException {
-        String header = "HTTP/1.1 200 OK\r\nContent-length: " + response.length() + "\r\n\r\n";
+
+        String header = "HTTP/1.1 200 OK\r\nContent-length: " + getResponseLength(response) + "\r\n\r\n";
         outputToClient.write(header.getBytes(StandardCharsets.UTF_8));
 
-        byte[] body = response.getBytes(StandardCharsets.UTF_8);
-        outputToClient.write(body);
-
+        if (response != null) {
+            byte[] body = response.getBytes(StandardCharsets.UTF_8);
+            outputToClient.write(body);
+        }
         outputToClient.flush();
+    }
+
+    public static int getResponseLength(String response) {
+        if (response == null) {
+            return 0;
+        } else return response.getBytes(StandardCharsets.UTF_8).length;
     }
 }
